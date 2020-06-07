c499f719c106bdedc41b58b95764b49acae67c18
Francisco Grande <franciscogrande91@gmail.com>
2020-06-07 19:47:18 -0300

    Arreglos en clase Vuelo

--- src/TPF/Vuelo.java
+++ src/TPF/Vuelo.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 public class Vuelo {
     private Date fecha;
-    private double costoKm;
     private TipoVuelo tipoVuelo;
     private Avion avion;
     private Set<Usuario> pasajeros;
@@ -14,16 +13,14 @@ public class Vuelo {
 
     public Vuelo() {
         this.fecha = new Date();
-        this.costoKm = 0;
         this.tipoVuelo = null;
         this.avion = null;
         this.pasajeros = new HashSet<Usuario>();
         this.cantPasajeros = pasajeros.size();
     }
 
-    public Vuelo(double costoKm, TipoVuelo tipoVuelo, Avion avion, int cantPasajeros) {
+    public Vuelo(TipoVuelo tipoVuelo, Avion avion, int cantPasajeros) {
         this.fecha = new Date();
-        this.costoKm = costoKm;
         this.tipoVuelo = tipoVuelo;
         this.avion = avion;
         this.pasajeros = new HashSet<Usuario>();
@@ -32,10 +29,6 @@ public class Vuelo {
 
     public Date getFecha() {
         return fecha;
-    }
-
-    public double getCostoKm() {
-        return costoKm;
     }
 
     public TipoVuelo getTipoVuelo() {
@@ -54,15 +47,11 @@ public class Vuelo {
         return cantPasajeros;
     }
 
-    public void setCostoKm(double costoKm) {
-        this.costoKm = costoKm;
-    }
-
     public void setAvion(Avion avion) {
         this.avion = avion;
     }
 
-//    public double costoTotal(){
-//        return (this.tipoVuelo.getDistancia() * this.costoKm) + (cantPasajeros * 3500) + (Avion.getTarifa);
-//    }
+    public double costoTotal(){
+       return (this.tipoVuelo.getDistancia() * avion.costoPorKm) + (cantPasajeros * 3500) + (avion.getTarifa());
+    }
 }
