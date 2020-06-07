f5fac53403d3632a7e0b6326f510851646f17d14
JorgePiaggio <proto2345@protonmail.com>
2020-06-06 23:36:00 -0300

    clase avion e hijos

--- src/TPF/Main.java
+++ src/TPF/Main.java
@@ -3,7 +3,8 @@ package TPF;
 public class Main {
 
     public static void main(String[] args) {
-
+        Gold Boeing = new Gold(123,1024.5,300,700,Propulsion.REACCION,true,true);
+        System.out.println(Boeing.toString());
 
     }
 }
